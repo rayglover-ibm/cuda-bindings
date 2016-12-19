dd86a3b6342fe14d377a452d20192afb1c2d674a
Ray Glover <ray.glover@uk.ibm.com>
2016-12-19 10:52:30 +0000

    rename

--- bindings/python/src/module.cpp
+++ bindings/python/src/module.cpp
@@ -4,7 +4,7 @@
 namespace py = pybind11;
 
 PYBIND11_PLUGIN(libsrpy) {
-    py::module m("libsrpy", "pybind11 example plugin");
+    py::module m("libsrpy", "python binding example");
 
     m.def("add", static_cast<int (*)(int, int)>(&libsr::add), "A function which adds two numbers");
 
