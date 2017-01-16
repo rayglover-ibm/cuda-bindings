49ee776a78f1bb1517beca789d4dff86d4fa9dce
Ray Glover <ray.glover@uk.ibm.com>
2017-01-16 14:44:52 +0000

    cmake tidy

--- cuda.cmake
+++ cuda.cmake
@@ -4,15 +4,15 @@ set (CUDA_VERBOSE_BUILD ON)
 find_package (CUDA REQUIRED)
 
 set (SRC_CUDA
-    src/add.cu
-    src/device_util.cu
+    "src/add.cu"
+    "src/device_util.cu"
 )
 cuda_include_directories (
-    ${CMAKE_CURRENT_SOURCE_DIR}/include
-    ${CMAKE_CURRENT_BINARY_DIR}/include
-    ${CMAKE_CURRENT_SOURCE_DIR}/src
+    "${CMAKE_CURRENT_SOURCE_DIR}/include"
+    "${CMAKE_CURRENT_BINARY_DIR}/include"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src"
 )
-cuda_add_library (${tgt}_cuda
+cuda_add_library (${core}_cuda
     STATIC ${SRC_CUDA}
     OPTIONS "-gencode arch=compute_30,code=sm_30 -cudart static"
 )
