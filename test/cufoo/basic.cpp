49070a34fe32e682b0d1cc63db470c8373988741
Ray Glover <ray.glover@uk.ibm.com>
2017-01-31 16:13:14 +0000

    multi-threaded unit test

--- test/cufoo/basic.cpp
+++ test/cufoo/basic.cpp
@@ -3,6 +3,8 @@
 #include "cufoo_config.h"
 
 #include <array>
+#include <vector>
+#include <thread>
 
 using std::array;
 
@@ -22,4 +24,28 @@ TEST(cufoo, add_span)
     
     array<int, 6> d{ 8, 10, 12, 14, 16, 18 };
     EXPECT_EQ(c, d);
+}
+
+TEST(cufoo, add_span_multithreaded)
+{
+    const uint32_t M = 1024000;
+
+    std::vector<int> a(M, 7);
+    std::vector<int> b(M, 21);
+    
+    auto fn = [&](int tid)
+    {
+        std::vector<int> c(M, 0);
+        cufoo::add(a, b, c);
+        for (int x : c) EXPECT_EQ(x, 7 + 21);
+    };
+
+    const uint32_t N = 4;
+    std::thread t[N];
+
+    for (int i = 0; i < N; ++i)
+        t[i] = std::thread(fn, i);
+
+    for (int i = 0; i < N; ++i)
+        t[i].join();
 }
