595731b6ce47381e02e31469bf5f06f0275f70ce
ray glover <ray.glover@uk.ibm.com>
2017-03-20 12:52:40 +0000

    [build] update compiler features

--- tests.cmake
+++ tests.cmake
@@ -1,11 +1,5 @@
 cmake_minimum_required (VERSION 3.2)
-
 include (CTest)
-
-# Turn on all warnings when using gcc.
-if (CMAKE_COMPILER_IS_GNUCXX)
-	add_definitions ("--std=c++14 -W -Wall -pedantic")
-endif ()
 
 # main test suite
 add_executable (mwe_test
@@ -16,7 +10,6 @@ target_link_libraries (mwe_test
 	mwe gtest gmock_main
 )
 
-add_test (
-	NAME mwe_test_suite
+add_test (NAME mwe_test_suite
 	COMMAND mwe_test
 )
